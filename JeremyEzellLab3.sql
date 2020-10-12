732a63c378d77b39ef2c4cb9901c6f1fbf7e8e1a
MaxwellV15 <72464516+MaxwellV15@users.noreply.github.com>
2020-10-12 13:43:00 -0400

    Add files via upload
    Updated with PBKDF2 Encryption, Stored Procedure and Encoding for cross site scripting - Missing parameterized queries

---
+++ JeremyEzellLab3.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE[dbo].[Auth]
+	@Email AS NCHAR(50),
+	@Password AS NCHAR(50)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT Email, Password
+	FROM UserInfo
+	Where Email = @Email
+	AND Password = @Password;
+
+	End
