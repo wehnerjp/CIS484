493225bd292df1c7425376bf957adc66d405d009
Hunter Rabon <rabonz671@gmail.com>
2020-10-24 13:50:34 -0400

    HunterChanges

---
+++ CIS484Solution1/WebForm1.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Configuration;
+using System.Windows.Forms;
+using System.Web.Services;
+
+namespace CIS484Solution1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+    }    
+}
