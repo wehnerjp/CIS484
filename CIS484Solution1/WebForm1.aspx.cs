587f1211416d10b9fa1fe34c9b7002098f6486e1
John Wehner <wehnerjp@dukes.jmu.edu>
2020-10-25 18:59:43 -0400

    Fixing Merge issues

--- CIS484Solution1/WebForm1.aspx.cs
+++ CIS484Solution1/WebForm1.aspx.cs
@@ -287,50 +287,6 @@ namespace CIS484Solution1
                 msg += ex.Message;
                 throw new Exception(msg);
             }
-        protected void SubmitCoordinator_Click(object sender, EventArgs e)
-        {
-            //Inserting teacher query
-            //Get connection string from web.config file
-            string ct = ConfigurationManager.ConnectionStrings["CyberDayMaster"].ConnectionString;
-            SqlConnection con = new SqlConnection(ct);
-
-            //Inserting Coordinator query
-
-            String sqlQuery1 = "Insert into CoordinatorAuth (CoordinatorID, Username, Password) values ((select CoordinatorID from Coordinator where CoordinatorID = (select Max(CoordinatorID) from Coordinator)), @Username, @Password)";
-            //Get connection string from web.config file
-            //create new sqlconnection and connection to database by using connection string from web.config file
-            SqlCommand cmd = new SqlCommand(sqlQuery1, con);
-            cmd.Parameters.Add(new SqlParameter("@Username", UsernameTextBox.Text));
-            cmd.Parameters.Add(new SqlParameter("@Password", PasswordHash.HashPassword(modalLRInput13.Text)));
-
-            String sqlQuery = "Insert into Coordinator (Name, Email, Phone) values (@Name, @Email, @Phone);";
-            SqlCommand command = new SqlCommand(sqlQuery, con);
-            command.Parameters.Add(new SqlParameter("@Name", CoordinatorNameText.Text));
-            command.Parameters.Add(new SqlParameter("@Email", EmailTextBox.Text));
-            command.Parameters.Add(new SqlParameter("@Phone", PhoneTextBox.Text));
-            con.Open(); 
-            try
-            {
-
-
-                command.ExecuteNonQuery();
-
-                //cmd.Parameters.Add(new SqlParameter("@Username", UsernameTextBox.Text));
-                //cmd.Parameters.Add(new SqlParameter("@Password", PasswordHash.HashPassword(modalLRInput13.Text)));
-
-
-                Console.Write("insert successful");
-                    //MessageBox.Show("insert teacher success");
-
-             }
-                catch (SqlException ex)
-                {
-                    Console.Write(ex.Message);
-                    lblStatus.Text = "Database Error";
-                }
-            try
-            {
-
 
             con.Close();
             PopulateSequence();
@@ -604,18 +560,55 @@ namespace CIS484Solution1
             PopulateSequence();
             RequestListDDLUpdate.Update();
         }
-    }
-}
+
+        protected void SubmitCoordinator_Click(object sender, EventArgs e)
+        {
+            //Inserting teacher query
+            //Get connection string from web.config file
+            string ct = ConfigurationManager.ConnectionStrings["CyberDayMaster"].ConnectionString;
+            SqlConnection con = new SqlConnection(ct);
+
+            //Inserting Coordinator query
+
+            String sqlQuery1 = "Insert into CoordinatorAuth (CoordinatorID, Username, Password) values ((select CoordinatorID from Coordinator where CoordinatorID = (select Max(CoordinatorID) from Coordinator)), @Username, @Password)";
+            //Get connection string from web.config file
+            //create new sqlconnection and connection to database by using connection string from web.config file
+            SqlCommand cmd = new SqlCommand(sqlQuery1, con);
+            cmd.Parameters.Add(new SqlParameter("@Username", UsernameTextBox.Text));
+            cmd.Parameters.Add(new SqlParameter("@Password", PasswordHash.HashPassword(modalLRInput13.Text)));
+
+            String sqlQuery = "Insert into Coordinator (Name, Email, Phone) values (@Name, @Email, @Phone);";
+            SqlCommand command = new SqlCommand(sqlQuery, con);
+            command.Parameters.Add(new SqlParameter("@Name", CoordinatorNameText.Text));
+            command.Parameters.Add(new SqlParameter("@Email", EmailTextBox.Text));
+            command.Parameters.Add(new SqlParameter("@Phone", PhoneTextBox.Text));
+            con.Open();
+            try
+            {
+                command.ExecuteNonQuery();
+
+                //cmd.Parameters.Add(new SqlParameter("@Username", UsernameTextBox.Text));
+                //cmd.Parameters.Add(new SqlParameter("@Password", PasswordHash.HashPassword(modalLRInput13.Text)));
+
+                Console.Write("insert successful");
+                //MessageBox.Show("insert teacher success");
+            }
+            catch (SqlException ex)
+            {
+                Console.Write(ex.Message);
+                lblStatus.Text = "Database Error";
+            }
+            try
+            {
                 cmd.CommandType = CommandType.Text;
                 cmd.ExecuteNonQuery();
                 ResetCoordinator_Click(sender, e);
                 //cmd.Parameters.Add(new SqlParameter("@Username", UsernameTextBox.Text));
                 //cmd.Parameters.Add(new SqlParameter("@Password", PasswordHash.HashPassword(modalLRInput13.Text)));
 
-
                 Console.Write("insert 2 successful");
                 //MessageBox.Show("insert teacher success");
-   
+
                 ResetCoordinator_Click(sender, e);
             }
             catch (System.Data.SqlClient.SqlException ex)
@@ -626,28 +619,25 @@ namespace CIS484Solution1
             }
             con.Close();
 
+            //con1.Close();
+        }
 
-                //con1.Close();
-
-            }
-        
-
-            protected void PopulateCoordinator_Click(object sender, EventArgs e)
-            {
-                Random rnd = new Random();
+        protected void PopulateCoordinator_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
             UsernameTextBox.Text = HttpUtility.HtmlEncode(CoordinatorNameText.Text);
-                EmailTextBox.Text = HttpUtility.HtmlEncode(UsernameTextBox.Text) + "@edu.com";
-                modalLRInput13.Text = "";
-            }
+            EmailTextBox.Text = HttpUtility.HtmlEncode(UsernameTextBox.Text) + "@edu.com";
+            modalLRInput13.Text = "";
+        }
 
-            protected void ResetCoordinator_Click(object sender, EventArgs e)
-            {
-                //clear teacher input
-                CoordinatorNameText.Text = string.Empty;
-                EmailTextBox.Text = string.Empty;
-                PhoneTextBox.Text = string.Empty;
-                UsernameTextBox.Text = string.Empty;
-                modalLRInput13.Text = string.Empty;
-            }
+        protected void ResetCoordinator_Click(object sender, EventArgs e)
+        {
+            //clear teacher input
+            CoordinatorNameText.Text = string.Empty;
+            EmailTextBox.Text = string.Empty;
+            PhoneTextBox.Text = string.Empty;
+            UsernameTextBox.Text = string.Empty;
+            modalLRInput13.Text = string.Empty;
         }
     }
+}
