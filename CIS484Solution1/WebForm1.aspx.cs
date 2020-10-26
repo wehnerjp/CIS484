d4f792663ab057f5e219e79c9b6f452b409d8f36
Hunter Rabon <rabonz671@gmail.com>
2020-10-25 20:29:14 -0400

    Insert statements work! Working on GridView

--- CIS484Solution1/WebForm1.aspx.cs
+++ CIS484Solution1/WebForm1.aspx.cs
@@ -16,8 +16,22 @@ namespace CIS484Solution1
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        
         public static AccessCode MasterAccessCode = new AccessCode();
 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection sqlconnect = new SqlConnection(ConfigurationManager.ConnectionStrings["CyberDayMaster"].ConnectionString);
+            sqlconnect.Open();
+
+            // Grid View Display - Initial
+            string sqlQuery1 = "Select InstructorCode, Name, Email, Phone from INSTRUCTOR";
+            SqlDataAdapter sqlAdapter = new SqlDataAdapter(sqlQuery1, sqlconnect);
+            DataTable In_GridView = new DataTable();
+            sqlAdapter.Fill(In_GridView);
+            Instructor_GridView.DataSource = In_GridView;
+            Instructor_GridView.DataBind();
+        }
         protected void ContactMenu_MenuItemClick(object sender, MenuEventArgs e)
         {
             System.Web.UI.WebControls.Menu ContactMenu = sender as System.Web.UI.WebControls.Menu;
@@ -27,38 +41,25 @@ namespace CIS484Solution1
 
         protected void SubmitButton_Click(object sender, EventArgs e)
         {
-            // Generate codes
-            string code = "";
-            code = MasterAccessCode.GenerateCode(lowercase: true, uppercase: true, numbers: true, otherChar: true, codeSize: 8);
-            MessageBox.Show(code.ToString(), "Code 1 for instructor: ");
+            // Generate Cluster and Instructor Codes
+            string clusterCode = "";
+            clusterCode = MasterAccessCode.GenerateCode(lowercase: true, uppercase: true, numbers: true, otherChar: true, codeSize: 8);
+            MessageBox.Show(clusterCode.ToString(), "Code 1 for instructor: ");
 
-            string code2 = "";
-            code2 = MasterAccessCode.GenerateCode(lowercase: true, uppercase: true, numbers: true, otherChar: true, codeSize: 8);
-            MessageBox.Show(code2.ToString(), "Code 2 for instructor: ");
+            string  instructorCode= "";
+            instructorCode = MasterAccessCode.GenerateCode(lowercase: true, uppercase: true, numbers: true, otherChar: true, codeSize: 8);
+            MessageBox.Show(instructorCode.ToString(), "Code 2 for instructor: ");
 
-            // Grid View Display
-            string sqlQuery1 = "Select InstructorCode, Name, Email, Phone from INSTRUCTOR";
             SqlConnection sqlconnect = new SqlConnection(ConfigurationManager.ConnectionStrings["CyberDayMaster"].ConnectionString);
-
-            //create new sqlconnection and connection to database by using connection string from web.config file
             sqlconnect.Open();
 
-            SqlDataAdapter sqlAdapter = new SqlDataAdapter(sqlQuery1, sqlconnect);
-
-            DataTable In_GridView = new DataTable();
-            sqlAdapter.Fill(In_GridView);
-            Instructor_GridView.DataSource = In_GridView;
-            Instructor_GridView.DataBind();
-
-            // INSERT SQL STATEMENTS
-
+            // Necessary information for insert statements
             string eventID = ""; 
             string contactCode = "";
             string orgID = "";
 
-            
+            // Find necessary information
             string sqlQuery2 = "SELECT EventID, ContactCode, OrganizationID FROM EVENTCONTACT";
-
             SqlCommand cmd100 = new SqlCommand(sqlQuery2, sqlconnect); 
 
             try
@@ -79,49 +80,18 @@ namespace CIS484Solution1
                 msg += ex.Message;
                 throw new Exception(msg); 
             }
-            
 
-            String sqlQuery3 = "INSERT INTO INSTRUCTOR(InstructorCOde, Name, OrganizationID, Email, Phone, ContactCode)" +
-                "VALUES (@InstructorCode, @Name, @OrganizationID, @Email, @Phone, @ContactCode)";
-            String sqlQuery4 = "INSERT INTO ACCESSCODE (Code, UserType)" +
-                "VALUES (@Code, @UserType)";
-            String sqlQuery5 = "INSERT INTO CLUSTER(ClusterCode, InstructorCode, OrganizationID)" +
-                "VALUES(@ClusterCode, @InstructorCode, @OrganizationID)";
+            // INSERT SQL Statements
 
-            SqlCommand cmd101 = new SqlCommand(sqlQuery3, sqlconnect);
-            SqlCommand cmd102 = new SqlCommand(sqlQuery4, sqlconnect);
-            SqlCommand cmd103 = new SqlCommand(sqlQuery5, sqlconnect);
-
-            cmd101.Parameters.Add(new SqlParameter("@InstructorCode", code2));
-            cmd101.Parameters.Add(new SqlParameter("@Name", Instructor_tbFirstName.Text + Instructor_tbLastName.Text));
-            cmd101.Parameters.Add(new SqlParameter("@OrganizationID", orgID));
-            cmd101.Parameters.Add(new SqlParameter("@Email", Instructor_tbEmail.Text));
-            cmd101.Parameters.Add(new SqlParameter("@Phone", Instructor_tbPhone.Text));
-            cmd101.Parameters.Add(new SqlParameter("@ContactCode", contactCode));
-            
-            cmd102.Parameters.Add(new SqlParameter("@Code", code2));
-            cmd102.Parameters.Add(new SqlParameter("@UserType", "Teacher"));
-
-            cmd103.Parameters.Add(new SqlParameter("@ClusterCode", code));
-            cmd103.Parameters.Add(new SqlParameter("@InstructorCode", code2));
-            cmd103.Parameters.Add(new SqlParameter("@OrganizationID", orgID));
-
+            // Insert - Access Part 1
+            String sqlQuery_p1 = "INSERT INTO ACCESSCODE(Code, UserType) VALUES (@Code, @UserType)";
+            SqlCommand cmd_p1 = new SqlCommand(sqlQuery_p1, sqlconnect);
+            cmd_p1.Parameters.Add(new SqlParameter("@Code", clusterCode));
+            cmd_p1.Parameters.Add(new SqlParameter("@UserType", "Instructor"));
             try
             {
-                cmd103.CommandType = CommandType.Text;
-                cmd103.ExecuteNonQuery();
-            }
-            catch (System.Data.SqlClient.SqlException ex)
-            {
-                string msg = "Insert Error into Cluster";
-                msg += ex.Message;
-                throw new Exception(msg);
-            }           
-
-            try
-            {
-                cmd102.CommandType = CommandType.Text;
-                cmd102.ExecuteNonQuery();
+                cmd_p1.CommandType = CommandType.Text;
+                cmd_p1.ExecuteNonQuery();
             }
             catch (System.Data.SqlClient.SqlException ex)
             {
@@ -130,6 +100,32 @@ namespace CIS484Solution1
                 throw new Exception(msg);
             }
 
+            // Insert - Cluster Part 1
+            String sqlQuery_p2 = "INSERT INTO CLUSTER(ClusterCode) VALUES (@ClusterCode)";
+            SqlCommand cmd_p2 = new SqlCommand(sqlQuery_p2, sqlconnect);
+            cmd_p2.Parameters.Add(new SqlParameter("@ClusterCode", clusterCode));
+            try
+            {
+                cmd_p2.CommandType = CommandType.Text;
+                cmd_p2.ExecuteNonQuery();
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
+                string msg = "Insert Error into Cluster Part 1";
+                msg += ex.Message;
+                throw new Exception(msg);
+            }
+
+            // Insert - Instructor
+            String sqlQuery4 = "INSERT INTO INSTRUCTOR(InstructorCode, Name, OrganizationID, Email, Phone, ContactCode)" +
+                "VALUES (@InstructorCode, @Name, @OrganizationID, @Email, @Phone, @ContactCode)";
+            SqlCommand cmd101 = new SqlCommand(sqlQuery4, sqlconnect);
+            cmd101.Parameters.Add(new SqlParameter("@InstructorCode", instructorCode));
+            cmd101.Parameters.Add(new SqlParameter("@Name", Instructor_tbFirstName.Text + ' ' + Instructor_tbLastName.Text));
+            cmd101.Parameters.Add(new SqlParameter("@OrganizationID", orgID));
+            cmd101.Parameters.Add(new SqlParameter("@Email", Instructor_tbEmail.Text));
+            cmd101.Parameters.Add(new SqlParameter("@Phone", Instructor_tbPhone.Text));
+            cmd101.Parameters.Add(new SqlParameter("@ContactCode", contactCode));
             try
             {
                 cmd101.CommandType = CommandType.Text;
@@ -140,9 +136,35 @@ namespace CIS484Solution1
                 string msg = "Insert Error into Instructor";
                 msg += ex.Message;
                 throw new Exception(msg);
+
             }
 
+            // Update - Cluster Part 2
+            String sqlQuery3 = "UPDATE Cluster SET InstructorCode = @InstructorCode, OrganizationID = @OrganizationID WHERE ClusterCode = @ClusterCode";
+            SqlCommand cmd103 = new SqlCommand(sqlQuery3, sqlconnect); 
+            cmd103.Parameters.Add(new SqlParameter("@InstructorCode", instructorCode));
+            cmd103.Parameters.Add(new SqlParameter("@OrganizationID", orgID));
+            cmd103.Parameters.Add(new SqlParameter("@ClusterCode", clusterCode));
+            try
+            {
+                cmd103.CommandType = CommandType.Text;
+                cmd103.ExecuteNonQuery();
+            }
+            catch (System.Data.SqlClient.SqlException ex)
+            {
 
+                string msg = "Insert Error into Cluster Part 2";
+                msg += ex.Message;
+                throw new Exception(msg);
+            }
+            
+            // Grid View Display - Update
+            string sqlQuery1 = "Select InstructorCode, Name, Email, Phone from INSTRUCTOR";
+            SqlDataAdapter sqlAdapter = new SqlDataAdapter(sqlQuery1, sqlconnect);
+            DataTable In_GridView = new DataTable();
+            sqlAdapter.Fill(In_GridView);
+            Instructor_GridView.DataSource = In_GridView;
+            Instructor_GridView.DataBind();
         }
 
         protected void Instructor_ResetButton_Click(object sender, EventArgs e)
@@ -153,6 +175,4 @@ namespace CIS484Solution1
             Instructor_tbPhone.Text = string.Empty;
         }
     }
-
-
 }
